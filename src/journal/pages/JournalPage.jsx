381d3aabdf2ac995e8610924902c0ade29b2885a
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-09-09 00:55:04 -0400

    Se creó el view para cuando no se tiene seleccionado ningún elemento del menú

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,10 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout >
-      <Typography variant="h1">JournalPage</Typography>
+      <NothingSelectedView />
     </JournalLayout>
   )
 }
