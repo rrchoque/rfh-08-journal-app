53f513255883e795277484859ecea397104a7253
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-09-09 00:04:50 -0400

    Se implementó layout en el componente Auth

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,27 +1,11 @@
 import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { Link as RouterLink } from 'react-router-dom';
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const LoginPage = () => {
   return (
-    <Grid 
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-    >
-      <Grid item
-        className='box-shadow'
-        xs={ 3 }
-        sx={{ 
-              width: { sm: 450 },
-              backgroundColor: 'white', 
-              padding: 3, 
-              borderRadius: 2 
-      }}>
-        <Typography variant='h5' sx={{ mb: 1 }}>Login</Typography>
+      <AuthLayout title="Login">
         <form>
           <Grid container>
             <Grid item xs={ 12 } sx={{ mt: 2 }}>
@@ -63,10 +47,7 @@ export const LoginPage = () => {
             </Grid>
 
           </Grid>
-
-
         </form>
-      </Grid>
-    </Grid>
+      </AuthLayout>
   )
 }
