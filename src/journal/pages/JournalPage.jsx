df79fd7349d19e1eb18db45dde2a9550b17c6f3c
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-09-09 00:35:58 -0400

    Se diseñó el layout para el Journal

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,10 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
+    <JournalLayout >
       <Typography variant="h1">JournalPage</Typography>
-    </>
-
+    </JournalLayout>
   )
 }
