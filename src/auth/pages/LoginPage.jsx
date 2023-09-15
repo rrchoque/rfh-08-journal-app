1b2b15687e1fa166d591122f65f7bf54f453f92e
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-09-14 23:48:00 -0400

    Se agregó el spinner para mostrar cuando se hace una petiición al servidor

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -14,8 +14,8 @@ export const LoginPage = () => {
   const dispatch = useDispatch();
 
   const { email, password, onInputChange } = useForm({
-    email: 'ricunidad@hotmail.com',
-    password: '123456'
+    email: '',
+    password: ''
   });
 
   const isAuthenticating = useMemo( () => status === 'checking', [status]);
