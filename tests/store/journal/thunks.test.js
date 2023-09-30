bd6e72cfc2c9ea69d1ebf1564545452945eedd7f
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-09-29 22:49:33 -0400

    Se terminaron de crear las pruebas de los thunks del journal

--- tests/store/journal/thunks.test.js
+++ tests/store/journal/thunks.test.js
@@ -1,5 +1,7 @@
+import { collection, deleteDoc, getDocs } from 'firebase/firestore/lite';
+import { FirebaseDB } from '../../../src/firebase/config';
+import { savingNewNote, addNewEmptyNote, setActiveNote } from '../../../src/store/journal/journalSlice';
 import { startNewNote } from '../../../src/store/journal/thunks';
-
 
 describe('Pruebas en Journal Thunks', () => {
 
@@ -8,8 +10,6 @@ describe('Pruebas en Journal Thunks', () => {
 
     beforeEach( () => jest.clearAllMocks() );
 
-
-
     test('startNewNote debe de crear una nueva nota en blanco', async() => {
 
         const uid = 'TEST-UID';
@@ -17,7 +17,27 @@ describe('Pruebas en Journal Thunks', () => {
 
         await startNewNote()( dispatch, getState );
 
-    });
+        expect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
+        expect( dispatch ).toHaveBeenCalledWith( addNewEmptyNote({
+            body: '',
+            title:'',
+            id: expect.any( String ),
+            date: expect.any( Number ),
+        }));
+        expect( dispatch ).toHaveBeenCalledWith( setActiveNote({
+            body: '',
+            title:'',
+            id: expect.any( String ),
+            date: expect.any( Number ),
+        }));
 
-    
+        // Borrar de firebase
+        const collectionRef = collection( FirebaseDB, `${ uid }/journal/notes`);
+        const docs = await getDocs( collectionRef );
+
+        const deletePromises = [];
+        docs.forEach( doc => deletePromises.push( deleteDoc( doc.ref ) ) );
+        await Promise.all( deletePromises );
+        
+    });
 });
