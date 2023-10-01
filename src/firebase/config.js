656a0f95729e0bf3614d883d3c7ac399dbf25d0c
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-09-30 23:41:18 -0400

    Se agregó la configuración de servidor firebase para test

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,25 @@ import { getFirestore } from 'firebase/firestore/lite';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// Dev/Prod
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBMB5ailiClH4ZH1JdgM3NYV03oBRIE1jU",
+//   authDomain: "fh-react-cursos-e63d7.firebaseapp.com",
+//   projectId: "fh-react-cursos-e63d7",
+//   storageBucket: "fh-react-cursos-e63d7.appspot.com",
+//   messagingSenderId: "10636071903",
+//   appId: "1:10636071903:web:cb7a346013e96e09a46a6a"
+// };
+
+// Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyBMB5ailiClH4ZH1JdgM3NYV03oBRIE1jU",
-  authDomain: "fh-react-cursos-e63d7.firebaseapp.com",
-  projectId: "fh-react-cursos-e63d7",
-  storageBucket: "fh-react-cursos-e63d7.appspot.com",
-  messagingSenderId: "10636071903",
-  appId: "1:10636071903:web:cb7a346013e96e09a46a6a"
+  apiKey: "AIzaSyBWnMmMZBuuNsmHtA99ElDI2BYuGLsVsRo",
+  authDomain: "fh-react-cursos-test.firebaseapp.com",
+  projectId: "fh-react-cursos-test",
+  storageBucket: "fh-react-cursos-test.appspot.com",
+  messagingSenderId: "328765789246",
+  appId: "1:328765789246:web:14e343885e67413b032d6f",
+  measurementId: "G-JVMPMBPJHK"
 };
 
 // Initialize Firebase
