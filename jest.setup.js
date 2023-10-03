c544adfe167ef171758c96260a4d40b765cac59c
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-10-03 00:37:48 -0400

    Se configuró la aplicación para utilizar las variables de entorno

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,10 @@
 // En caso de necesitar la implementación del FetchAPI
 import 'whatwg-fetch'; // <-- yarn add whatwg-fetch
+
+require('dotenv').config({
+    path: '.env.test'
+});
+
+jest.mock('./src/helpers/getEnvironments', () => ({
+    getEnvironments: () => ({ ...process.env })
+}));
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,8 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
+import { getEnvironments } from "../helpers";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+  VITE_MESUREMENTID,
+} = getEnvironments();
 
 // Your web app's Firebase configuration
 // Dev/Prod
@@ -18,13 +29,13 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 // Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyBWnMmMZBuuNsmHtA99ElDI2BYuGLsVsRo",
-  authDomain: "fh-react-cursos-test.firebaseapp.com",
-  projectId: "fh-react-cursos-test",
-  storageBucket: "fh-react-cursos-test.appspot.com",
-  messagingSenderId: "328765789246",
-  appId: "1:328765789246:web:14e343885e67413b032d6f",
-  measurementId: "G-JVMPMBPJHK"
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
+  measurementId: VITE_MESUREMENTID,
 };
 
 // Initialize Firebase
