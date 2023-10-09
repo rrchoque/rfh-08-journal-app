b8cb072f70b0360a3bf36b53e1ab9b7f4c46b2ac
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-10-09 00:00:02 -0400

    Test de ventana de login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -85,6 +85,7 @@ export const LoginPage = () => {
                   disabled={ isAuthenticating }
                   variant='contained' 
                   fullWidth
+                  aria-label="google-btn"
                   onClick={ onGoogleSignIn }>
                   <Google />
                   <Typography sx={{ ml: 1 }}>Google</Typography>
---
+++ tests/auth/pages/LoginPage.test.jsx
@@ -0,0 +1,56 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { LoginPage } from '../../../src/auth/pages/LoginPage';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { authSlice } from '../../../src/store/auth';
+import { MemoryRouter } from 'react-router-dom';
+
+const mockStartGoogleSignIn = jest.fn();
+
+jest.mock('../../../src/store/auth/thunks', () => ({
+    startGoogleSignIn: () => mockStartGoogleSignIn,
+    startLoginWithEmailPassword: ({ email, password }) => {
+        return () => mockStartLoginWithEmailPassword({ email, password });
+    },
+}));
+
+const store = configureStore({
+    reducer: {
+        auth: authSlice.reducer
+    },
+    // preloadedState: {
+    //     auth: notAuthenticatedState
+    // }
+})
+
+describe('Pruebas en <LoginPage />', () => {
+
+    test('debe de mostrar el componente correctamente', () => {
+
+        render(
+            <Provider store={ store }>
+                <MemoryRouter>
+                    <LoginPage />
+                </MemoryRouter>
+            </Provider>
+        );
+
+        expect( screen.getAllByText('Login').length ).toBeGreaterThanOrEqual(1);
+    });
+
+    test('boton de google debe de llamar el startGoogleSignIn', () => { 
+
+        render(
+            <Provider store={ store }>
+                <MemoryRouter>
+                    <LoginPage />
+                </MemoryRouter>
+            </Provider>
+        );
+
+        const googleBtn = screen.getByLabelText('google-btn');
+        fireEvent.click( googleBtn );
+        expect( mockStartGoogleSignIn ).toHaveBeenCalled();
+
+    });
+});
